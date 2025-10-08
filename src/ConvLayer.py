ae4f786e57128031c86254d68e6aee288d0000c7
snb71e <seonae@dgist.ac.kr>
2025-10-08 20:20:17 +0900

    refactor

--- src/ConvLayer.py
+++ src/ConvLayer.py
@@ -4,12 +4,12 @@ from src.matrix import im2col, col2im
 class ConvLayer:
     def __init__(self, input_channels, output_channels, filter_h, filter_w, stride=1, padding=0):
 
-        self.FN = output_channels
-        self.C = input_channels
+        self.C_in = input_channels
+        self.C_out = output_channels
         self.FH = filter_h
         self.FW = filter_w
-        self.S = stride
-        self.P = padding
+        self.stride = stride
+        self.padding = padding
 
         scale = np.sqrt(2.0 / (input_channels * filter_h * filter_w))
         self.W = scale * np.random.randn(output_channels, input_channels, filter_h, filter_w)
@@ -20,33 +20,31 @@ class ConvLayer:
         self.db = None
 
     def pad(self, x):
-        if self.P == 0:
+        if self.padding == 0:
             return x
-        return np.pad(x, ((0, 0), (0, 0), (self.P, self.P), (self.P, self.P)), 'constant', constant_values=0)
+        return np.pad(x, ((0, 0), (0, 0), (self.padding, self.padding), (self.padding, self.padding)), 'constant', constant_values=0)
     
     def forward(self, x):
         N, C, H, W = x.shape
-        FH, FW, S, P = self.FH, self.FW, self.S, self.P
 
-        cols, OH, OW = im2col(x, FH, FW, S, P)
-        W_col = self.W.reshape(self.FN, -1)
+        cols, H_out, W_out = im2col(x, self.FH, self.FW, self.stride, self.padding)
+        W_col = self.W.reshape(self.C_out, -1)
         out = W_col @ cols + self.b[:, None] 
-        out = out.reshape(self.FN, N, OH, OW).transpose(1, 0, 2, 3)
-        self.cache = (x.shape, cols, OH, OW, W_col)
+        out = out.reshape(self.C_out, N, H_out, W_out).transpose(1, 0, 2, 3)
+        self.cache = (x.shape, cols, H_out, W_out, W_col)
 
         return out
     
-    def backward(self, upstream_grad):
-        x_shape, cols, OH, OW, W_col = self.cache
+    def backward(self, grad_output):
+        x_shape, cols, H_out, W_out, W_col = self.cache
         N, C, H, W = x_shape
-        FN, C, FH, FW = self.W.shape
-        S, P = self.S, self.P
+        C_out, C_in, FH, FW = self.W.shape
 
-        dout2 = upstream_grad.transpose(1, 0, 2, 3).reshape(FN, N * OH * OW)
+        dout_col = grad_output.transpose(1, 0, 2, 3).reshape(C_out, N * H_out * W_out)
 
-        self.db = dout2.sum(axis=1)
-        self.dW = (dout2 @ cols.T).reshape(self.W.shape)
+        self.db = dout_col.sum(axis=1)
+        self.dW = (dout_col @ cols.T).reshape(self.W.shape)
 
-        dcols = W_col.T @ dout2
-        dx = col2im(dcols, x_shape, FH, FW, stride=S, pad=P)
-        return dx
+        dcols = W_col.T @ dout_col
+        grad_input = col2im(dcols, x_shape, FH, FW, stride=self.stride, pad=self.padding)
+        return grad_input
